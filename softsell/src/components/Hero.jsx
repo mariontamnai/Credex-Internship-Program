d2947d295b26305469ca7ae793c70d2fe03aa43b
Marion <mariontamnai@gmail.com>
2025-05-11 09:53:40 +0300

    adde new lines

--- softsell/src/components/Hero.jsx
+++ softsell/src/components/Hero.jsx
@@ -2,5 +2,9 @@ const Hero = () => (
     <section className="bg-blue-100 text-center py-20 px-4">
         <h1 className="text-4xl font-bold">Turn Unused Software Into Cash</h1>
         <p className="mt-4 text-lg">Easily resell your software license and get paid quickly.</p>
+        <button className="mt-6 bg-blue-600 text-white px-6 py-3 roumded-md hover:bg-blue-700">
+            Sell My Licenses
+        </button>
     </section>
-)
+);
+export default Hero;
